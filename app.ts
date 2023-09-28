468ac2e6cd8140d3a236cf78b9062e2c4b90a7fa
Maurício Ricardo <43684435+mauriciocostaol@users.noreply.github.com>
2023-09-28 16:26:55 -0300

    Adicionando enum

--- app.ts
+++ app.ts
@@ -9,23 +9,42 @@
 // console.log(`nome ${nome} ${sobrenome}, idade ${idade}`);
 
 
-type TipoArray = (number | "TreinaWeb")[];
+// type TipoArray = (number | "TreinaWeb")[];
 
-let a: TipoArray =[1,2,3,"TreinaWeb"];
-let b: TipoArray = ["TreinaWeb","TreinaWeb",4];
+// let a: TipoArray =[1,2,3,"TreinaWeb"];
+// let b: TipoArray = ["TreinaWeb","TreinaWeb",4];
 
-console.log(a,b);
+// console.log(a,b);
 
-type Animal = {
-    nome:string;
-    idade:number;
+// type Animal = {
+//     nome:string;
+//     idade:number;
+// }
+
+// let cachorro: Animal;
+
+// cachorro = {
+//     nome: "Dog",
+//     idade:5
+// }
+
+// console.log(cachorro);
+
+enum produtoStatus{
+    
+}
+function checarProdutoStatus(status:number){
+    switch(status){
+        case 1:
+            console.log("Produto Disponível")
+            break;
+        case 2:
+            console.log("Produto Não Encontrado");
+            break;
+        case 3:
+            console.log("Produto aguardando estoque");
+            break;
+    }
 }
 
-let cachorro: Animal;
-
-cachorro = {
-    nome: "Dog",
-    idade:5
-}
-
-console.log(cachorro);
+checarProdutoStatus(2);
